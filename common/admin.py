f38fd330844f4146ce19ca7ef45df5761fb8ee0f
Sardor-program <sardorali378@gmail.com>
2024-05-17 16:00:41 +0500

    common admin register

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from common.models import *
+
+admin.site.register(Media)
+admin.site.register(Settings)
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(OurInstagramStory)
+
+class CustomerFeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_name', 'customer_position', 'rank')
+    search_fields = ('customer_name', 'customer_position')
+    list_filter = ('rank')
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.sitel.register(CustomerFeedback, CustomerFeedbackAdmin)
+        
+
+
+
+
+
